8c3789ed3dc70402c0f1a1b499f3678630cee960
Nick Lauerman <nicholas.lauerman@gmail.com>
2019-02-08 12:23:44 -0600

    finished word cloud

--- working.R
+++ working.R
@@ -1,24 +1,25 @@
-set.seed(12345)
 
+set.seed(1)
 require(RColorBrewer)
 
-pal <- brewer.pal(9,"BuGn")
-pal <- pal[-(1:4)]
+
+#pal <- brewer.pal(9,"BuGn")
+#pal <- pal[-(1:4)]
 
 pal <- brewer.pal(6,"Dark2")
 pal <- pal[-(1)]
            
            
 jpeg(filename = "summer.jpeg",
-     width = 5,
-     height = 5,
+     width = 6,
+     height = 6,
      units = "in",
      res = 600,
      quality = 90)
 wordcloud(words=names(wordFreq.1st),
           freq = wordFreq.1st,
           min.freq=50,
-          random.order = F,
+          random.order = FALSE,
           random.color = TRUE,
           colors = pal)
 dev.off()
